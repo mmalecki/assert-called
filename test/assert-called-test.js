956d232e5cae0184c9f4f024fba25cc2de0d01e6
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-09-22 11:38:25 +0200

    [test] Add tests for stack trace

--- test/assert-called-test.js
+++ test/assert-called-test.js
@@ -6,7 +6,8 @@ process.nextTick(cb(function called() {}));
 
 process.on('uncaughtException', function (ex) {
   assert(ex instanceof assert.AssertionError);
-  assert.equal(ex.message, '1 callback not called:\n  notCalled');
+  assert.equal(ex.message.indexOf('1 callback not called:\n  notCalled'), 0);
+  assert.notEqual(ex.message.indexOf('assert-called-test.js:4:1'), -1);
   process.exit(0);
 });
 
